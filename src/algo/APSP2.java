75540a23dc59b58662d1e202658580c90904024d
tsogtbayar <tsogtoo.wow@gmail.com>
2017-08-30 08:01:19 +0800

    done

--- src/algo/APSP2.java
+++ src/algo/APSP2.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class APSP2 {
@@ -13,49 +14,146 @@ public class APSP2 {
 	int nVert,nEdge;
 	ArrayList<ArrayList<Integer>> vertices;
 	ArrayList<int[]> edges;
+	boolean check=false;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		APSP2 obj=new APSP2();
-		obj.readFile("g1");
-		int A[][]=obj.bellmanFord(1);
+		obj.readFile("g3");
+		int res[][]=new int[obj.nVert][obj.nVert];
+		boolean cycle=false;
+		int min=Integer.MAX_VALUE;
 		for(int i=0;i<obj.nVert;i++) {
+			int A[][]=obj.bellmanFord(i+1);
+			if(obj.check==true)
+				break;
+			for(int j=0;j<obj.nVert;j++) {
+				if(min>A[obj.nVert][j]) {
+					min=A[obj.nVert][j];
+				}
+			}
 			
+			
+/*			if(!Arrays.equals(A[obj.nVert],A[obj.nVert-1])) {
+				cycle=true;
+				break;
+			}
+			
+			if(cycle==true) {
+				System.out.println("neg cycle");
+				break;
+			}				
+*/			
+			System.out.println(i);
 		}
 		
-//		for(int i=0;i<obj.vertices.size()-900;i++) {
-//			System.out.print(i+": ");
-//			for(int j:obj.vertices.get(i)) {
-//				System.out.print(j+" ");
-//			}
-//			System.out.println();
-//		}
+		System.out.println(min);
+/*		int A1[][]=obj.bellmanFord(1);
+		int A2[][]=obj.bellmanFord(2);
+		int A3[][]=obj.bellmanFord(3);
+		int A4[][]=obj.bellmanFord(4);
+		for(int i=0;i<obj.nVert;i++) {
+			System.out.print(A1[obj.nVert][i]+" ");
+		}
+		System.out.println();
+		for(int i=0;i<obj.nVert;i++) {
+			System.out.print(A2[obj.nVert][i]+" ");
+		}
+		System.out.println();
+		for(int i=0;i<obj.nVert;i++) {
+			System.out.print(A3[obj.nVert][i]+" ");
+		}
+		System.out.println();
+		for(int i=0;i<obj.nVert;i++) {
+			System.out.print(A4[obj.nVert][i]+" ");
+		}
+*/		
+		
+/*		for(int i=0;i<obj.vertices.size();i++) {
+			System.out.print(i+1+": ");
+			for(int j:obj.vertices.get(i)) {
+				System.out.print(j+2+" ");
+			}
+			System.out.println();
+		}*/
 		
 	}
 	public int[][] bellmanFord(int s) {
-		int A[][]=new int[nVert-1][nVert];
+		int A[][]=new int[nVert+1][nVert];
 		for(int i=0;i<nVert;i++) {
 			if(s-1==i) {
 				A[0][s-1]=0;
 			}
 			else
-				A[0][i]=99999;
+				A[0][i]=Integer.MAX_VALUE;
 		}
 		
-		for(int i=1;i<=nVert-1;i++) {
+/*		for(int i=1;i<=nVert-1;i++) {
 			for(int j=0;j<nEdge;j++) {
-//				if(A[i-1][edges.get(j)[0]]!=99999) {
-					if(A[i-1][edges.get(j)[1]]>A[i-1][edges.get(j)[0]+edges.get(j)[2]]) {
-						A[i][edges.get(j)[1]]=A[i-1][edges.get(j)[0]+edges.get(j)[2]];
+				if(A[i-1][edges.get(j)[0]-1]!=99999) {
+					if(A[i-1][edges.get(j)[1]-1]>A[i-1][edges.get(j)[0]-1]+edges.get(j)[2]) {
+						A[i][edges.get(j)[1]-1]=A[i-1][edges.get(j)[0]-1]+edges.get(j)[2];
+					}
+					else
+						A[i][edges.get(j)[1]-1]=A[i-1][edges.get(j)[1]-1];
+				}					
+			}			
+		}*/
+		
+		
+		
+		
+//		System.out.println(vertices.get(0).get(1));
+		
+		for(int i=1;i<=nVert-1;i++) {
+			for(int j=0;j<nVert;j++) {
+				int min=Integer.MAX_VALUE;
+				for(int e:vertices.get(j)) {
+					if(j==edges.get(e)[1]-1) {
+						if(A[i-1][edges.get(e)[0]-1]!=Integer.MAX_VALUE&&min>A[i-1][edges.get(e)[0]-1]+edges.get(e)[2]) {
+							min=A[i-1][edges.get(e)[0]-1]+edges.get(e)[2];
+						}
 					}					
-//				}
+				}
+				if(min>A[i-1][j]) {
+					A[i][j]=A[i-1][j];
+				}
+				else
+					A[i][j]=min;
 			}
 			
 		}
+
+		for(int j=0;j<nVert;j++) {
+			int min=Integer.MAX_VALUE;
+			for(int e:vertices.get(j)) {
+				if(j==edges.get(e)[1]-1) {
+					if(A[nVert-1][edges.get(e)[0]-1]!=Integer.MAX_VALUE&&min>A[nVert-1][edges.get(e)[0]-1]+edges.get(e)[2]) {
+						min=A[nVert-1][edges.get(e)[0]-1]+edges.get(e)[2];
+					}
+				}					
+			}
+			if(min>A[nVert-1][j]) {
+				A[nVert][j]=A[nVert-1][j];
+			}
+			else {
+				A[nVert][j]=min;
+			}
+			
+			if(A[nVert][j]!=A[nVert-1][j]) {
+				check=true;
+				System.out.println("neg cycle");
+				break;
+				
+			}
+			
+		}
+		
+		
 		return A;
 	}
 	public void readFile(String filename) {
 		try {
-			fr = new FileReader("C:\\Users\\tsogtbayarn\\Documents\\Workspace\\java\\" + filename + ".txt");
+			fr = new FileReader("/home/stark/Documents/Workspace/java/" + filename + ".txt");
 			br = new BufferedReader(fr);
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
